e68b10843ce92023de54cb82d8edf44d2d77e100
GEINER MAURICIO RIVERA JIMENEZ <geiner.rivera@uao.edu.co>
2021-03-06 10:51:56 -0500

    Terminada parte de Calcular, esperando respuesta de aceptacion

--- paqueteria-app/app/src/main/java/com/gdrivera/paqueteria_app/MainActivity.java
+++ paqueteria-app/app/src/main/java/com/gdrivera/paqueteria_app/MainActivity.java
@@ -1,14 +1,26 @@
 package com.gdrivera.paqueteria_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button miBoton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        miBoton =(Button)findViewById(R.id.btnCalcular);
+        miBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,calcular_activity.class); // Creo intent que referencie la primera y segunda ventana
+                startActivity(i);// Activo la intención que se va a enviar
+            }
+        });
     }
 }
